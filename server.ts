d95be8c1f45bf2002426622587f1323fa4c3c469
Leo <leo.maedje@gmail.com>
2022-09-21 15:44:23 +0200

    Wrote basic GET route handlers

--- server.ts
+++ server.ts
@@ -23,14 +23,77 @@ const app = express();
 app.use(express.json()); //add body parser to each following route handler
 app.use(cors()) //add CORS support to each following route handler
 
-const client = new Client(dbConfig);
+const client = new Client("resourcedb");
 client.connect();
 
-app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
-  res.json(dbres.rows);
-});
+// GET /resources //get all resources
+app.get("/resources", async (req, res) => {
+  try {
+    const response = await client.query("select * from resources order by time_date desc");
+    res.json(response.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({message: "Ooops"});
+  }
+})
 
+// GET /resources/:res-id //get a given resource
+app.get("/resources/:resID", async (req, res) => { //add type later
+  const {resID} = req.params;
+  try {
+    const response = await client.query("select * from resources where resource_id = $1", [parseInt(resID)]);
+    res.json(response.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({message: "Ooops"});
+  }
+})
+
+// GET /resources/:res-id/comments //get all comments for a resource
+app.get("/resources/:resID/comments", async (req, res) => {
+  const {resID} = req.params
+  try {
+    const response = await client.query("select * from comments where resource_id = $1", [parseInt(resID)]);
+    res.json(response.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({message: "Ooops"});
+  }
+})
+
+// GET /tags //get all the tags
+app.get("/tags", async (req, res) => {
+  try {
+    const response = await client.query("select * from tags");
+    res.json(response.rows);
+  } catch(error) {
+    console.error(error);
+    res.status(400).json({message: "Ooops"});
+  }
+})
+
+// GET /users //get all the users
+app.get("/users", async (req, res) => {
+  try {
+    const response = await client.query("select * from users");
+    res.json(response.rows);
+  } catch(error) {
+    console.error(error);
+    res.status(400).json({message: "Ooops"});
+  }
+})
+
+// GET /users/:user-id/study-list //get user's study list
+app.get("/users/:userID/study-list", async (req, res) => {
+  const {userID} = req.params;
+  try {
+    const response = await client.query("select * from study_list where user_id = $1", [parseInt(userID)]);
+    res.json(response.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({message: "Surprise!"});
+  }
+})
 
 //Start the server on the given port
 const port = process.env.PORT;
