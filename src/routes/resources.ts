6a2f90de1e4593b080bf05e87b497cdd2e30ed09
niamhbrockbank <niamh.brockbank@gmail.com>
2022-12-02 12:17:08 +0000

    Format SQL queries consistently

--- src/routes/resources.ts
+++ src/routes/resources.ts
@@ -62,7 +62,12 @@ export function setUpRouter(client : Client): Router{
     resourcesRouter.get<{comment_id: number}>("/comments/:comment_id/likes", async (req, res) => {
         const {comment_id} = req.params;
         try {
-        const dbResponse = await client.query(`select liked, count(*) from comment_likes where comment_id = $1 group by (liked);`, [comment_id]);
+        const dbResponse = await client.query(`
+          SELECT liked, count(*) 
+          FROM comment_likes 
+          WHERE comment_id = $1 
+          GROUP BY (liked);`
+          , [comment_id]);
         res.status(200).json(dbResponse.rows);
         } catch (error) {
         console.error(error);
@@ -75,18 +80,30 @@ export function setUpRouter(client : Client): Router{
     resourcesRouter.post<{}, {}, IResourceSubmit>("/", async (req, res) => {
         const {resource_name, author_name, url, description, content_type, rating, notes, user_id, tag_array} = req.body;    
         try {
-          const dbResponse = await client.query(`INSERT INTO resources (resource_name, author_name, url, description, content_type, rating, notes, user_id) 
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`, 
+          const dbResponse = await client.query(`
+            INSERT INTO resources 
+            (resource_name, author_name, url, description, content_type, rating, notes, user_id) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8) 
+            RETURNING *`, 
             [resource_name, author_name, url, description, content_type, rating, notes, user_id]);
+
           const {resource_id} = dbResponse.rows[0];
-          const existingTags = await client.query(`SELECT * from tags`);
+
+          const existingTags = await client.query(`SELECT * FROM tags`);
           const existingTagNames = existingTags.rows.map(tagRow => tagRow.tag_name);
           const newTags = tag_array.map(postedTag => postedTag.tag_name).filter(newTag => !existingTagNames.includes(newTag));
+
           for (const newTag of newTags) {
-            await client.query(`INSERT INTO tags VALUES ($1)`, [newTag]);
+            await client.query(`
+              INSERT INTO tags 
+              VALUES ($1)`
+              , [newTag]);
           }
           for (const tag of tag_array) {
-            await client.query(`INSERT INTO resource_tags VALUES ($1, $2)`, [resource_id, tag.tag_name]);
+            await client.query(`
+              INSERT INTO resource_tags 
+              VALUES ($1, $2)`
+              , [resource_id, tag.tag_name]);
           }
           // await axios.post(process.env.DISCORD_URL,
           //   {content: `There's a new resource (${dbResponse.rows[0].resource_name}) on ${frontEndURL}!`});
@@ -102,7 +119,12 @@ export function setUpRouter(client : Client): Router{
         const {res_id} = req.params;
         const {comment_body, user_id} = req.body;
         try {
-          const dbResponse = await client.query(`INSERT INTO comments (comment_body, user_id, resource_id) VALUES ($1, $2, $3) RETURNING *`, [comment_body, user_id, res_id]);
+          const dbResponse = await client.query(`
+            INSERT INTO comments 
+            (comment_body, user_id, resource_id) 
+            VALUES ($1, $2, $3) 
+            RETURNING *`
+            , [comment_body, user_id, res_id]);
           res.status(201).json(dbResponse.rows);
         } catch (error) {
           console.error(error);
@@ -117,9 +139,10 @@ export function setUpRouter(client : Client): Router{
         const like_boolean = like_or_dislike === "like" ? true : false;
         try {
           const dbResponse = await client.query(`
-          INSERT INTO resource_likes VALUES ($1,$2,$3) 
-          ON CONFLICT (resource_id, user_id) 
-          DO UPDATE SET liked = $3 RETURNING *; `, [res_id, user_id, like_boolean]);
+            INSERT INTO resource_likes VALUES ($1,$2,$3) 
+            ON CONFLICT (resource_id, user_id) 
+            DO UPDATE SET liked = $3 RETURNING *; `
+            , [res_id, user_id, like_boolean]);
           res.status(200).json(dbResponse);
         } catch (error) {
           console.error(error);
@@ -134,9 +157,10 @@ export function setUpRouter(client : Client): Router{
         const like_boolean = like_or_dislike === "like" ? true : false;
         try {
           const dbResponse = await client.query(`
-          INSERT INTO comment_likes VALUES ($1,$2,$3) 
-          ON CONFLICT (comment_id, user_id) 
-          DO UPDATE SET liked = $3 RETURNING *; `, [comment_id, user_id, like_boolean]);
+            INSERT INTO comment_likes VALUES ($1,$2,$3) 
+            ON CONFLICT (comment_id, user_id) 
+            DO UPDATE SET liked = $3 RETURNING *; `
+            , [comment_id, user_id, like_boolean]);
           res.status(200).json(dbResponse);
         } catch (error) {
           console.error(error);
@@ -150,20 +174,41 @@ export function setUpRouter(client : Client): Router{
         const {res_id} = req.params
         const {resource_name, author_name, url, description, content_type, rating, notes, user_id, tag_array} = req.body;    
         try {
-            const dbResponse = await client.query(`UPDATE resources 
-            SET resource_name=$1, author_name=$2, url=$3, description=$4, content_type=$5, 
-            rating=$6, notes=$7, user_id=$8 WHERE resource_id=$9 RETURNING *`, 
-            [resource_name, author_name, url, description, content_type, rating, notes, user_id, res_id]);
+            const dbResponse = await client.query(`
+              UPDATE resources 
+              SET 
+                resource_name=$1, 
+                author_name=$2, 
+                url=$3, 
+                description=$4, 
+                content_type=$5, 
+                rating=$6, 
+                notes=$7, 
+                user_id=$8 
+              WHERE resource_id=$9 
+              RETURNING *`, 
+              [resource_name, author_name, url, description, content_type, rating, notes, user_id, res_id]);
             const {resource_id} = dbResponse.rows[0];
-            await client.query(`DELETE FROM resource_tags WHERE resource_id = $1`, [resource_id]);
-            const existingTags = await client.query(`SELECT * from tags`);
+            await client.query(`
+              DELETE FROM resource_tags 
+              WHERE resource_id = $1`
+              , [resource_id]);
+            const existingTags = await client.query(`
+              SELECT * 
+              FROM tags`);
             const existingTagNames = existingTags.rows.map(tagRow => tagRow.tag_name);
             const newTags = tag_array.map(postedTag => postedTag.tag_name).filter(newTag => !existingTagNames.includes(newTag));
             for (const newTag of newTags) {
-            await client.query(`INSERT INTO tags VALUES ($1)`, [newTag]);
+            await client.query(`
+              INSERT INTO tags 
+              VALUES ($1)`
+              , [newTag]);
             }
             for (const tag of tag_array) {
-            await client.query(`INSERT INTO resource_tags VALUES ($1, $2)`, [resource_id, tag.tag_name]);
+            await client.query(`
+              INSERT INTO resource_tags 
+              VALUES ($1, $2)`
+              , [resource_id, tag.tag_name]);
             }
             if (dbResponse.rowCount === 1) {
             res.status(201).json(dbResponse.rows);
@@ -181,7 +226,12 @@ export function setUpRouter(client : Client): Router{
         const {comment_id} = req.params;
         const {comment_body} = req.body;
         try {
-            const dbResponse = await client.query(`UPDATE comments SET comment_body=$1 WHERE comment_id=$2 RETURNING *`, [comment_body, comment_id])
+            const dbResponse = await client.query(`
+              UPDATE comments 
+              SET comment_body=$1 
+              WHERE comment_id=$2 
+              RETURNING *`
+              , [comment_body, comment_id])
             if (dbResponse.rowCount === 1) {
             res.status(200).json(dbResponse.rows);
             } else {
@@ -198,7 +248,11 @@ export function setUpRouter(client : Client): Router{
     resourcesRouter.delete<{res_id: number}>("/resources/:res_id", async (req, res) => {
         const {res_id} = req.params;
         try {
-            const dbResponse = await client.query("delete from resources where resource_id = $1 returning *", [res_id]);
+            const dbResponse = await client.query(`
+              DELETE FROM resources 
+              WHERE resource_id = $1 
+              RETURNING *`
+              , [res_id]);
             if (dbResponse.rowCount === 1) {
             res.status(200).json({status: "success", message: `Deleted resource ${res_id}`})
             } else {
@@ -214,7 +268,11 @@ export function setUpRouter(client : Client): Router{
     resourcesRouter.delete<{comment_id: number}>("/resources/comments/:comment_id", async (req, res) => {
         const {comment_id} = req.params;
         try {
-        const dbResponse = await client.query("delete from comments where comment_id = $1 returning *", [comment_id]);
+        const dbResponse = await client.query(`
+          DELETE FROM comments 
+          WHERE comment_id = $1 
+          RETURNING *`
+          , [comment_id]);
         if (dbResponse.rowCount === 1) {
             res.status(200).json({status: "success", message: `Deleted comment ${comment_id}`})
         } else {
@@ -231,7 +289,12 @@ export function setUpRouter(client : Client): Router{
         const {res_id} = req.params;
         const {user_id} = req.params;
         try {
-        const dbResponse = await client.query("delete from resource_likes where resource_id = $1 and user_id=$2 returning *", [res_id, user_id]);
+        const dbResponse = await client.query(`
+          DELETE FROM resource_likes 
+          WHERE resource_id = $1 
+          AND user_id=$2 
+          RETURNING *`
+          , [res_id, user_id]);
         if (dbResponse.rowCount === 1) {
             res.status(200).json({status: "success", message: `Deleted your like/dislike from ${res_id}`})
         } else {
@@ -248,7 +311,12 @@ export function setUpRouter(client : Client): Router{
         const {comment_id} = req.params;
         const {user_id} = req.body;
         try {
-        const dbResponse = await client.query("delete from comment_likes where comment_id = $1 and user_id=$2 returning *", [comment_id, user_id]);
+        const dbResponse = await client.query(`
+          DELETE FROM comment_likes 
+          WHERE comment_id = $1 
+          AND user_id=$2 
+          RETURNING *`
+          , [comment_id, user_id]);
         if (dbResponse.rowCount === 1) {
             res.status(200).json({status: "success", message: `Deleted your like/dislike from ${comment_id}`})
         } else {
