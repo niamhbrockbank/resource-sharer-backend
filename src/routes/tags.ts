d70ecef1d30b8ed2f7fa3c14ba1a0ee9485c3d78
niamhbrockbank <niamh.brockbank@gmail.com>
2022-12-02 12:18:39 +0000

    Format SQL queries consistently

--- src/routes/tags.ts
+++ src/routes/tags.ts
@@ -8,7 +8,9 @@ export function setUpRouter(client : Client): Router{
     // GET all the tags
     tagsRouter.get("/", async (req, res) => {
       try {
-        const dbResponse = await client.query("select * from tags");
+        const dbResponse = await client.query(`
+          SELECT * 
+          FROM tags`);
         res.status(200).json(dbResponse.rows);
       } catch(error) {
         console.error(error);
@@ -21,7 +23,11 @@ export function setUpRouter(client : Client): Router{
     tagsRouter.delete<{}, {}, {tag_name: string}>("/", async (req, res) => {
       const {tag_name} = req.body;
       try {
-        const dbResponse = await client.query("delete from tags where tag_name = $1 returning *", [tag_name]);
+        const dbResponse = await client.query(`
+          DELETE FROM tags 
+          WHERE tag_name = $1 
+          RETURNING *`
+          , [tag_name]);
         if (dbResponse.rowCount === 1) {
           res.status(200).json({status: "success", message: `Deleted the tag ${tag_name}`});
         } else {
